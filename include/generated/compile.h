dc027097b46469eccaa128627359aa3d26097be6
Ralph Hempel <rhempel@hempeldesigngroup.com>
2013-11-22 10:58:46 -0500

    Remove auto-generated files from git repo, add to .gitignore

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 4 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Sat Nov 9 13:37:41 EST 2013"
-#define LINUX_COMPILE_BY "rhempel"
-#define LINUX_COMPILE_HOST "emdebian"
-#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
