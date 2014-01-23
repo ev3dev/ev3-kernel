f3ed3d3431536670fd352063c333ea373bcf5933
David Lechner <david@lechnology.com>
2014-01-23 12:12:49 -0600

    legoev3-uart: use hrtimer and tasklet for keep-alive

--- include/linux/legoev3/legoev3_uart.h
+++ include/linux/legoev3/legoev3_uart.h
@@ -16,6 +16,54 @@
 #ifndef _LINUX_LEGOEV3_UART_H_
 #define _LINUX_LEGOEV3_UART_H_
 
+#define LEGOEV3_UART_NAME_SIZE	11
+#define LEGOEV3_UART_UNITS_SIZE	4
+#define LEGOEV3_UART_MODE_MAX	7
+
+enum legoev3_uart_data_type {
+	LEGOEV3_UART_DATA_8,
+	LEGOEV3_UART_DATA_16,
+	LEGOEV3_UART_DATA_32,
+	LEGOEV3_UART_DATA_FLOAT,
+};
+
+/**
+ * struct legoev3_uart_sensor_mode_info
+ * @mode: The id of this mode. (0-7)
+ * @name: The name of this mode
+ * @raw_min: The minimum raw value of the data read.
+ * @raw_min: The maximum raw value of the data read.
+ * @pct_min: The minimum percentage value of the data read.
+ * @pct_min: The maximum percentage value of the data read.
+ * @si_min: The minimum scaled value of the data read.
+ * @si_min: The maximum scaled value of the data read.
+ * @units: Units of the scaled value.
+ * @data_sets: Number of data points in DATA message.
+ * @format: Format of data in DATA message.
+ * @figures: Number of digits that should be displayed, including decimal point.
+ * @decimals: Decimal point position.
+ * @raw_data: Raw data read from the sensor.
+ *
+ * All of the min an max values are actually 32-bit float data type,
+ * but kernel drivers don't do floating point, so we use unsigned instead.
+ */
+struct legoev3_uart_mode_info {
+	u8 mode;
+	char name[LEGOEV3_UART_NAME_SIZE + 1];
+	unsigned raw_min;
+	unsigned raw_max;
+	unsigned pct_min;
+	unsigned pct_max;
+	unsigned si_min;
+	unsigned si_max;
+	char units[LEGOEV3_UART_UNITS_SIZE + 1];
+	u8 data_sets;
+	enum legoev3_uart_data_type format;
+	u8 figures;
+	u8 decimals;
+	u8 raw_data[32];
+};
+
 /**
  * struct legoev3_uart_sensor_platform_data
  * @tty: The TTY device this sensor is connected to.
@@ -30,5 +78,8 @@ struct legoev3_uart_sensor_platform_data {
 	u8 num_modes;
 	u8 num_view_modes;
 };
+
+extern int legoev3_uart_get_mode(struct tty_struct *tty);
+extern int legoev3_uart_set_mode(struct tty_struct *tty, const u8 mode);
 
 #endif /* _LINUX_LEGOEV3_UART_H_ */
