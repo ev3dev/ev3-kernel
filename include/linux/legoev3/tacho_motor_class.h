cd38cebc3ec4f9376ca189e3788aee0c301767d3
Ralph Hempel <rhempel@hempeldesigngroup.com>
2014-03-12 17:19:04 -0400

    Added getters and setters for speed, power, time, step

--- include/linux/legoev3/tacho_motor_class.h
+++ include/linux/legoev3/tacho_motor_class.h
@@ -18,14 +18,65 @@
 
 #include <linux/device.h>
 
+enum
+{
+  FORWARD,
+  BACKWARD,
+  BRAKE,
+  COAST,
+};
+
+enum
+{
+  UNLIMITED_UNREG,
+  UNLIMITED_REG,
+  LIMITED_REG_STEPUP,
+  LIMITED_REG_STEPCONST,
+  LIMITED_REG_STEPDOWN,
+  LIMITED_UNREG_STEPUP,
+  LIMITED_UNREG_STEPCONST,
+  LIMITED_UNREG_STEPDOWN,
+  LIMITED_REG_TIMEUP,
+  LIMITED_REG_TIMECONST,
+  LIMITED_REG_TIMEDOWN,
+  LIMITED_UNREG_TIMEUP,
+  LIMITED_UNREG_TIMECONST,
+  LIMITED_UNREG_TIMEDOWN,
+  LIMITED_STEP_SYNC,
+  LIMITED_TURN_SYNC,
+  LIMITED_DIFF_TURN_SYNC,
+  SYNCED_SLAVE,
+  RAMP_DOWN_SYNC,
+  HOLD,
+  BRAKED,
+  STOP_MOTOR,
+  IDLE,
+  NUM_TACHO_MOTOR_MODES,
+};
+
 struct tacho_motor_device {
 	int (*get_tacho    )(struct tacho_motor_device *);
+	int (*get_step     )(struct tacho_motor_device *);
+	int (*get_time     )(struct tacho_motor_device *);
 	int (*get_direction)(struct tacho_motor_device *);
 	int (*get_speed    )(struct tacho_motor_device *);
 	int (*get_power    )(struct tacho_motor_device *);
+	int (*get_state    )(struct tacho_motor_device *);
 
 	int  (*get_target_power    )(struct tacho_motor_device *);
 	void (*set_target_power    )(struct tacho_motor_device *, long target_power);
+
+	int  (*get_target_tacho    )(struct tacho_motor_device *);
+	void (*set_target_tacho    )(struct tacho_motor_device *, long target_tacho);
+
+	int  (*get_target_step     )(struct tacho_motor_device *);
+	void (*set_target_step     )(struct tacho_motor_device *, long target_step);
+
+	int  (*get_target_speed    )(struct tacho_motor_device *);
+	void (*set_target_speed    )(struct tacho_motor_device *, long target_speed);
+
+	int  (*get_target_time     )(struct tacho_motor_device *);
+	void (*set_target_time     )(struct tacho_motor_device *, long target_time);
 
 	/* private */
 	struct device dev;
