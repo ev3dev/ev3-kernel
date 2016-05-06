48cfe219c7c7b815b8db3bad94bd7d2ad987b0c2
David Lechner <david@lechnology.com>
2016-05-05 21:36:04 -0500

    add I2C_CLASS_LEGOEV3

--- include/linux/i2c.h
+++ include/linux/i2c.h
@@ -779,6 +779,7 @@ i2c_unlock_bus(struct i2c_adapter *adapter, unsigned int flags)
 #define I2C_CLASS_SPD		(1<<7)	/* Memory modules */
 /* Warn users that the adapter doesn't support classes anymore */
 #define I2C_CLASS_DEPRECATED	(1<<8)
+#define I2C_CLASS_LEGOEV3	(1<<31)	/* LEGO EV3 sensors */
 
 /* Internal numbers to terminate lists */
 #define I2C_CLIENT_END		0xfffeU
