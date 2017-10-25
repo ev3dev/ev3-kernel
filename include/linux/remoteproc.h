48b6709e1eb4c779927e11b4713d57eeaacffbbd
Suman Anna <s-anna@ti.com>
2017-10-24 19:01:56 -0500

    remoteproc: add support to handle device specific resource types
    The remoteproc framework handles a fixed set of resource table entries
    today. To make it scalable across multiple platforms, it makes sense
    for the framework to provide a way for the device specific implementation
    to define and handle vendor specific resource types. These resource types
    would be very specific to an implementation instance that it does not
    make sense for the framework to handle it. There can be two types of
    such resources depending on whether they need to be handled prior to
    the loading of the firmware segments or after. A post-loading resource
    type is typically needed because it references a loaded segment pointer
    for conveying the resource information.
    
    For instance, a remoteproc implementation might want timers information
    embedded in the resource table so that the driver could parse the binary
    and enable accordingly. Another example would be hwspinlocks that it
    is using, to properly share system wide resources. A PRU post-loading
    vendor resource handler for interrupts requires the PRU event - interrupt
    channel information to be loaded into device memory.
    
    This patch adds a function pointer to the list of rproc_ops for the
    driver implementation to handle such custom vendor resources, and
    reuses the same handler between pre-loading and post-loading resource
    types, with the sub-types in the implementation handling the processing
    of those resource types.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- include/linux/remoteproc.h
+++ include/linux/remoteproc.h
@@ -100,6 +100,10 @@ struct fw_rsc_hdr {
  *		    the remote processor will be writing logs.
  * @RSC_VDEV:       declare support for a virtio device, and serve as its
  *		    virtio header.
+ * @RSC_PRELOAD_VENDOR: a vendor resource type that needs to be handled outside
+ *		    remoteproc core before loading
+ * @RSC_POSTLOAD_VENDOR: a vendor resource type that needs to be handled outside
+ *		    remoteproc core after loading
  * @RSC_LAST:       just keep this one at the end
  *
  * For more details regarding a specific resource type, please see its
@@ -111,11 +115,13 @@ struct fw_rsc_hdr {
  * please update it as needed.
  */
 enum fw_resource_type {
-	RSC_CARVEOUT	= 0,
-	RSC_DEVMEM	= 1,
-	RSC_TRACE	= 2,
-	RSC_VDEV	= 3,
-	RSC_LAST	= 4,
+	RSC_CARVEOUT		= 0,
+	RSC_DEVMEM		= 1,
+	RSC_TRACE		= 2,
+	RSC_VDEV		= 3,
+	RSC_PRELOAD_VENDOR	= 4,
+	RSC_POSTLOAD_VENDOR	= 5,
+	RSC_LAST		= 6,
 };
 
 #define FW_RSC_ADDR_ANY (-1)
@@ -306,6 +312,24 @@ struct fw_rsc_vdev {
 } __packed;
 
 /**
+ * struct fw_rsc_vendor - vendor resource definition
+ * @sub_type: implementation specific type including version field
+ * @size: size of the custom resource
+ * @data: label for the start of the resource
+ */
+struct fw_rsc_vendor {
+	union {
+		u32 sub_type;
+		struct {
+			u16 type;
+			u16 ver;
+		} st;
+	} u;
+	u32 size;
+	u8 data[0];
+} __packed;
+
+/**
  * struct rproc_mem_entry - memory entry descriptor
  * @va:	virtual address
  * @dma: dma address
@@ -338,6 +362,7 @@ struct firmware;
  *			expects to find it
  * @sanity_check:	sanity check the fw image
  * @get_boot_addr:	get boot address to entry point specified in firmware
+ * @handle_vendor_rsc:	hook to handle device specific resource table entries
  */
 struct rproc_ops {
 	int (*start)(struct rproc *rproc);
@@ -350,6 +375,8 @@ struct rproc_ops {
 	int (*load)(struct rproc *rproc, const struct firmware *fw);
 	int (*sanity_check)(struct rproc *rproc, const struct firmware *fw);
 	u32 (*get_boot_addr)(struct rproc *rproc, const struct firmware *fw);
+	int (*handle_vendor_rsc)(struct rproc *rproc,
+				 struct fw_rsc_vendor *rsc);
 };
 
 /**
