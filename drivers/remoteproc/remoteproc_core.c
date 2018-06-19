59ff7b5b34d019c622453d4d13c8b832c154097e
David Lechner <david@lechnology.com>
2018-06-19 14:50:36 -0500

    remoteproc: add page parameter to da_to_va
    This adds a new page parameter to the da_to_va callback for remoteproc
    devices. This parameter will be used by devices that have more than
    one memory map, such as the PRU found in TI Sitara SoCs.
    
    On these devices, the same physical memory address can refer to two
    different locations, i.e. instruction memory or data memory. So, an
    extra bit of information is needed in the da_to_va callback to tell
    these apart.
    
    Devices that only have a single memory map will just pass 0 for this
    parameter. For now we are using 0 everywhere, but later patches will
    modify this value.
    
    Signed-off-by: David Lechner <david@lechnology.com>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -145,6 +145,7 @@ static void rproc_disable_iommu(struct rproc *rproc)
  * @rproc: handle of a remote processor
  * @da: remoteproc device address to translate
  * @len: length of the memory region @da is pointing to
+ * @page: memory page for processors that have more than one memory map
  *
  * Some remote processors will ask us to allocate them physically contiguous
  * memory regions (which we call "carveouts"), and map them to specific
@@ -169,13 +170,13 @@ static void rproc_disable_iommu(struct rproc *rproc)
  * here the output of the DMA API for the carveouts, which should be more
  * correct.
  */
-void *rproc_da_to_va(struct rproc *rproc, u64 da, int len)
+void *rproc_da_to_va(struct rproc *rproc, u64 da, int len, int page)
 {
 	struct rproc_mem_entry *carveout;
 	void *ptr = NULL;
 
 	if (rproc->ops->da_to_va) {
-		ptr = rproc->ops->da_to_va(rproc, da, len);
+		ptr = rproc->ops->da_to_va(rproc, da, len, page);
 		if (ptr)
 			goto out;
 	}
@@ -470,7 +471,7 @@ static int rproc_handle_trace(struct rproc *rproc, struct fw_rsc_trace *rsc,
 	}
 
 	/* what's the kernel address of this resource ? */
-	ptr = rproc_da_to_va(rproc, rsc->da, rsc->len);
+	ptr = rproc_da_to_va(rproc, rsc->da, rsc->len, 0);
 	if (!ptr) {
 		dev_err(dev, "erroneous trace resource entry\n");
 		return -EINVAL;
@@ -1183,7 +1184,7 @@ static void rproc_coredump(struct rproc *rproc)
 		phdr->p_flags = PF_R | PF_W | PF_X;
 		phdr->p_align = 0;
 
-		ptr = rproc_da_to_va(rproc, segment->da, segment->size);
+		ptr = rproc_da_to_va(rproc, segment->da, segment->size, 0);
 		if (!ptr) {
 			dev_err(&rproc->dev,
 				"invalid coredump segment (%pad, %zu)\n",
